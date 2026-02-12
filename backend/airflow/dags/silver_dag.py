c2fdab564a0e56722b5d0f309fadcdff42f2f212
Ricardo Perera <ricardoperera@Ricardos-MacBook-Pro.local>
2026-02-12 17:37:44 +0100

    fix/dag schedule to @daily

--- backend/airflow/dags/silver_dag.py
+++ backend/airflow/dags/silver_dag.py
@@ -80,8 +80,8 @@ def clean_staging_tables(upsert_results: dict) -> dict:
 
 
 @dag(
-    schedule=None,
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    schedule="@daily",
+    start_date=pendulum.datetime(2026, 1, 1, tz="UTC"),
     catchup=False,
     tags=["bronze_to_silver"],
 )
