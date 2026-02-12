33210fa10c88203b1ceb51a302dcd61c17dae970
Ricardo Perera <ricardoperera@Ricardos-MacBook-Pro.local>
2026-02-12 13:24:43 +0100

    fix/changing producer config to generate random data from random 30 days before

--- producer.py
+++ producer.py
@@ -1,11 +1,17 @@
+import random
+
 from backend.kafka.producer.service import Producer, ProducerSettings
 from backend.db_sql.managers.bronze_layer import BronzeManager, DatabaseCredentials
 from fake_data_generator import Generator
 
 
-def create_transactions(size: int) -> list[dict]:
+def create_transactions(
+        size: int,
+        n_days_before: int = 0,
+        random_days_before: list[int] | None = None,
+) -> list[dict]:
     return Generator().generate_multiple_transactions(
-        size=size, return_dict=True
+        size=size, return_dict=True, n_days_before=n_days_before, random_day_before=random_days_before
     )
 
 def produce_messages():
@@ -13,7 +19,10 @@ def produce_messages():
     manager = BronzeManager(credentials=db_creds)
     manager.start()
 
-    transactions = create_transactions(size=100)
+    total_days_before = 31
+    random_days_before = [i * -1 for i in range(0, total_days_before)]
+
+    transactions = create_transactions(size=1000, random_days_before=random_days_before)
     payloads = [
         {
             "transaction_details": transaction,
